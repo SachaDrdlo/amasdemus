a0714c19778d0c18a0729998b765099c6628fdbc
Dordolo Sacha <dordolosacha@gmail.com>
2021-04-06 19:10:03 +0200

    start header component

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,22 +1,32 @@
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image'
 import styles from '../../styles/components/Home.module.scss';
 
-
 const Header = () => {
+
+    const [opened, setOpened] = useState(false);
+    const breakpoint = 992;
+
+    const getSearch = () => {
+        setOpened(!opened);
+    }
+
     return (
         <div className="container">
-            <Grid container spacing={3} className={styles.header_container}>
-                    <Grid item xs={6} className={styles.header_container_left}>
-                        <div className={styles.header_container_left_logo}>
-                            <img src="../img/logos/horizontal-green.png" alt="" />
-                        </div>
-                    </Grid>
-                    <Grid item xs={6} className={styles.header_container_right}>
-                        <img src="../img/icons/search-icon.svg" alt="" />
+            <div className={styles.header_container}>
+                <div className={styles.header_container_left}>
+                    <div className={styles.header_container_left_logo}>
+                        <Image src="/img/logos/horizontal-green.png" width={164} height={32} layout="responsive" alt="" />
+                    </div>
+                </div>
+                <div xs={6} className={styles.header_container_right}>
+                    <div className={`${styles.header_container_right_inputcontainer} ${opened ? styles.active : null}`}>
+                        <img src="/img/icons/search-icon.svg" alt="" onClick={() => getSearch()} />
                         <input type="text" placeholder="rechercher une bière" />
-                    </Grid>
-            </Grid>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
