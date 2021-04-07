78a5c638cf594122964ed20aa8f92234c26fbc19
Dordolo Sacha <dordolosacha@gmail.com>
2021-04-07 16:59:43 +0200

    beer + blob illu and infos display

--- pages/components/BeerIllu.js
+++ pages/components/BeerIllu.js
@@ -1,17 +1,31 @@
+import { Grid } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/styles'
 import React from 'react'
 import styles from '../../styles/components/Home.module.scss';
 
-const BeerIllu = ({image, title}) => {
+const BeerIllu = ({ image, name, type, level }) => {
     return (
-        <div className={styles.container_illu}>
-            <div className={styles.container_illu_img}>
-                <h1>{title}</h1>
-                <img src={`../img/beers/redimensionedGrand/${image}`} alt="" />
-                <img src="" alt="" />
-            </div>
+        <Grid container className={styles.container_grid} justify="space-between">
+            <Grid item xs={12} sm={7} className={styles.container_grid_illu}>
+                <div className={styles.container_grid_illu_beer}>
+                    <img src={`../img/beers/redimensionedGrand/${image}`} alt="" />
+                </div>
+                <div className={styles.container_grid_illu_blob}>
+                    <img src="./img/blob.svg" alt="" />
+                </div>
+            </Grid>
+            <Grid item xs={12} sm={3} className={styles.container_grid_content}>
+                <p>La star de la semaine</p>
+                <hr />
+                <h1>{name}</h1>
+                <div></div>
+                <p>
+                    <span>{type} - </span>
+                    <span>{level}°</span>
+                </p>
+            </Grid>
+        </Grid>
 
-        </div>
     )
 }
 
--- pages/components/HomePage.js
+++ pages/components/HomePage.js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BeerIllu from './BeerIllu'
 import styles from '../../styles/components/Home.module.scss';
 import beers from '../../beers.json'
+import { Grid } from '@material-ui/core';
+
+
 
 const HomePage = () => {
 
-    console.log(beers);
-    const randomBeer = beers.map((beer) => {
-        console.log(beer);
+    const randomBeerImg = beers.map((beer) => {
         return (
             <BeerIllu
                 key={beer.id}
                 image={beer.image}
-                title={beer.name}
+                name={beer.name}
+                type={beer.type}
+                level={beer.level}
             />
         )
 
-    })
+    }).slice(0, 1)
 
     return (
-        <div className={styles.container}>
+        <div className="container">
             <div className={styles.hero_container}>
                 <div className={styles.hero_container_logo}>
                     <img src="../img/logos/picto-green.png" alt="" />
@@ -30,8 +33,7 @@ const HomePage = () => {
                     <p>Pour les amateurs de terroir</p>
                 </div>
             </div>
-
-            {randomBeer}
+            {randomBeerImg}
         </div>
     )
 }
