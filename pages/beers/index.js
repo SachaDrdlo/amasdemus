5a01555f0dc337f56f44f619412f2e78275b7544
Julien Debrauwer <ju.debrauwer@gmail.com>
2021-04-10 19:17:54 +0200

    filter feature behaviour (open filters on click) filter feature basic scss install material ui dependencies for testing accordion component

--- pages/beers/index.js
+++ pages/beers/index.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
 import styles from '../../styles/components/Beers.module.scss'
 import Header from '../components/Header'
 import Filters from '../components/Filters'
 
-export default function Beers({}){
+export default function Beers(){
 
+    const [opened, setOpened] = useState(true);
+    const breakpoint = 992;
 
+    const getFilters = (e) => {
+        e.currentTarget.id === 'filter' ? setOpened(!opened) : null
+    }
+
+    useEffect(() => {
+        document.body.classList.add("filters");
+        document.body.classList.add("filters__active", opened);
+        
+      },[opened])
    
     return (
         <div className={styles.beerspage}>
             <Header />
             <main className="container">
-                <div className={styles.filters_container}>
-                    <button className={styles.filters_btn}>
-                        <img id="search" src="/img/icons/filter-icon.svg" alt="" onClick={(e) => getSearch(e)} />
+                <div className={`${styles.filters_container} ${opened ? styles.filters_container_active : null}`}>
+                    <button id="filter" className={styles.filters_btn} onClick={(e) => getFilters(e)}>
+                        <img src="/img/icons/filter-icon.svg" alt=""/>
                         <p>Filtres</p>
                     </button>
                 </div>
+                <Filters />
             </main>
         </div>
     )
--- pages/components/Filters.js
+++ pages/components/Filters.js
@@ -1,13 +1,40 @@
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../styles/components/Filters.module.scss';
-
-
+// import FiltersAccordion from './FiltersAccordion';
 
 export default function Filters() {
+
+    const [opened, setOpened] = useState(true);
+    const breakpoint = 992;
+
+    const getFilters = (e) => {
+        e.currentTarget.id === 'filtercross' ? setOpened(!opened) : null
+    }
+
+    useEffect(() => {
+        document.body.classList.remove("filters__active", opened);
+      },[opened])
     return (
-        <div className="container">
-           
+        <div className="filters-lateralblock">
+            <div className="container">
+                <div className={styles.filters_container}>
+                    <div className={styles.filters_close}>
+                        <button className={styles.filters_close_btn} id="filtercross"  onClick={(e) => getFilters(e)}>
+                            <svg className={styles.filters_close_img} xmlns="http://www.w3.org/2000/svg"><path d="M20 4L4 20M4 4l16 16" /></svg>
+                        </button>
+                    </div>
+
+                    {/* <ul className={styles.filters_list}>
+                        <li className={styles.filters_list_el}>Type</li>
+                        <li className={styles.filters_list_el}>Provenance</li>
+                        <li className={styles.filters_list_el}>Saveurs</li>
+                    </ul> */}
+
+                    {/* <FiltersAccordion /> */}
+                    
+                </div>    
+            </div>
         </div>
     )
 }
