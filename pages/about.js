f26957fc82404d58fa40747eaf88fdbab6687b4f
Julien Debrauwer <ju.debrauwer@gmail.com>
2021-04-22 13:41:26 +0200

    updated about page content

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ const About = () => {
             type={'brune'}
             level={59}
             brewery={'Hauts-de-France'}
-            desc_brewery={"Berceau de nos 3 ingrédients exceptionnels, cette région habrite également les locaux de M2i formation, brasserie du savoir dans laquelle le maître brasseur Frédéric Girod apporte une attention toute particulière à la confection & à réussite de la bière Zytholoteam. Sa sagesse et ses conseils avisés ont rendu possible la création de ce projet."}
+            desc_brewery={"Berceau de nos 3 ingrédients exceptionnels, cette région habrite également les locaux de M2i formation, brasserie du savoir dans laquelle le maître brasseur Frédéric Girod apporte une attention toute particulière à la confection et à la réussite de la bière Zytholoteam. Sa sagesse et ses conseils avisés ont rendu possible la création de ce projet."}
             // brewery_id={beerData.id_brewery}
             flavours={'Créativité, force de caractère, passion'}
             // format={beerData.format}
@@ -26,7 +26,7 @@ const About = () => {
             image={'../img/beer-about.png'}
             img_brewery={'../img/hauts-de-france.png'}
             title={"L'alliance parfaite"}
-            description={"Chacun des ingrédients que composent cette bière a été brassé avec amour pendant 9 mois. Sacha, le houblonné Picard, Étienne, l'orge Dunkerquois et Julien, le malt Roubaisien forment à eux trois, l'Amas Demus. Cette bière, alliant de belles saveurs créatives, une technique affinée et beaucoup de caractère, saura vous emerveiller."}
+            description={"Chacun des ingrédients qui composent cette bière a été brassé avec amour pendant 9 mois. Sacha, le houblonné Picard, Étienne, l'orge Dunkerquois et Julien, le malt Roubaisien forment à eux trois, la Zytholoteam. Cette bière, alliant de belles saveurs créatives, une technique affinée et beaucoup de caractère, saura vous emerveiller."}
             />
             <Footer />
             <Navbar />
